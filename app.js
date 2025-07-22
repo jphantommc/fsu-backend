a328ef7edb82453dce159ddecb5c3deb6602c2c0
Jonan Osorio <jonanosorio20@gmail.com>
2025-07-21 22:16:00 -0400

    finished up on building the API's for serving the data, add/remove records, update exsisting records for the mini capstone

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import express from "express";
+import departmentsRouter from "./api/departments.js";
+import professorsRouter from "./api/professors.js";
 const app = express();
+app.use(express.json());
+
+app.use("/api/departments", departmentsRouter);
+app.use("/api/professors", professorsRouter);
 export default app;
+
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import app from "#app";
 import db from "#db/client";
 
+const cors = require("cors");
+app.use(cors({ origin: /localhost/ }));
+
 const PORT = process.env.PORT ?? 3000;
 
 await db.connect();
@@ -8,3 +11,4 @@ await db.connect();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
